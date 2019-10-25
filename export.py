e43fcd76cf29c7286441a880846f6c5933f9b654
daniel <daniel@163.com>
2019-10-25 11:25:45 +0700

    增加自动建立include文件夹

--- export.py
+++ export.py
@@ -7,12 +7,15 @@ __author__ = 'Daniel'
 
 import re
 import sys
+import os
 
-path='./proto/'
+proto_path = './proto/'
+include_path = './include/'
 
-def write_proto_hrl():
-    text = '%% 协议号宏定义\n'
-    file_name = path + 'test.proto'
+
+def _write_proto_hrl(file_name):
+    text = '%% ' + file_name + ' 协议号宏定义\n'
+    file_name = proto_path + file_name
     rfile = open(file_name)
     print('read file: ' + file_name)
     while 1:
@@ -34,11 +37,13 @@ def write_proto_hrl():
                 if next_line[0:7] == 'message':
                     if next_line[7:10] == 'c2s' or next_line[7:10] == 's2c':
                         define_name = next_line[7:-1]
-                        line_text = '-define(' + define_name + ', ' + define_value + ').\n'
+                        line_text = '-define(' + define_name + \
+                            ', ' + define_value + ').\n'
                         # print line_text
                         text = text + line_text
                     else:
-                        print('协议号:' + define_value + ' 后跟的message: ' + next_line[7:-1] + ' 名字错误! ')
+                        print('协议号:' + define_value + ' 后跟的message: ' +
+                              next_line[7:-1] + ' 名字错误! ')
                         sys.exit(0)
                 else:
                     print('协议号:' + define_value + ' 未跟着message!')
@@ -52,5 +57,12 @@ def write_proto_hrl():
     print('导出成功!!!')
 
 
+def _mkdir(path):
+    folder = os.path.exists(path)
+    if not folder:
+        os.makedirs(path)
+
+
 if __name__ == '__main__':
-    write_proto_hrl()
+    _mkdir(include_path)
+    _write_proto_hrl('test.proto')
